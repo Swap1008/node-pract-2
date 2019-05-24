c5d475eba3f1246c3ff9cf312584bdb30f4f9d4d
DigvijaySS <suryawanshidigvijay@gmail.com>
2019-05-24 20:16:48 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT||3000;
+const path=require('path');
+// Middleware
+app.set('view-engine','hbs');
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// Routes
+app.get('/',(req,res)=>{
+    res.render('index.hbs');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs');
+});
+
+// Listen
+
+app.listen(port,()=>{
+    console.log(`Server Started at ${port}`);
+    
+});
